accb0087ba881d1b825d466691b161485cc49f03
Daniel Harit <daniel.harit@gmail.com>
2022-03-15 20:00:40 +0200

    Integrate with mail sending by credit card service

--- payments-service/api/routes.js
+++ payments-service/api/routes.js
@@ -7,4 +7,6 @@ module.exports = function(app) {
         .get(controller.get_card_info);
     app.route('/card/:userid')
         .put(controller.update_card);
+    app.route('/card/resend/:userid')
+        .get(controller.resend_card_details);
 };
