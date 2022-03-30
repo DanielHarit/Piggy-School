08487d33d4462d64b40d20ff504321a5ca5fca22
Daniel Harit <daniel.harit@gmail.com>
2022-03-30 16:42:49 +0300

    Initial login and logout made

--- piggy-client/src/components/Router/Router.jsx
+++ piggy-client/src/components/Router/Router.jsx
@@ -1,5 +1,6 @@
 import { React } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from '../../views/LoginPage';
 import ParentHomePage from '../../views/ParentHomePage';
 import ChildHomePage from '../../views/ChildHomePage';
 import ParentTransferMoneyPage from '../../views/ParentTransferMoneyPage';
@@ -7,10 +8,11 @@ import ParentTransferMoneyPage from '../../views/ParentTransferMoneyPage';
 const Router = () => {
 	return (
 		<Routes>
+			<Route path='/login' element={<LoginPage />} />
 			<Route path='/parent' element={<ParentHomePage />} />
 			<Route path='/child' element={<ChildHomePage />} />
 			<Route path='/parentTransfer' element={<ParentTransferMoneyPage />} />
-			<Route path='*' element={<Navigate to={'/parent'} />} />
+			<Route path='*' element={<Navigate to={'/login'} />} />
 		</Routes>
 	);
 };
---
+++ piggy-client/src/views/LoginPage/LoginPage.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Login from '../../components/Auth/Login';
+import Logout from '../../components/Auth/Logout';
+
+const LoginPage = () => {
+
+  return (
+      <div>
+          <Login />
+          <Logout />
+      </div>
+  )
+}
+
+export default LoginPage;
