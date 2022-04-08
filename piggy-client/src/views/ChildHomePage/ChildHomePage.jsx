58c80e64125d3fd11613d5c759160e428a3e117d
Lee Meiri <leemeiri1@gmail.com>
2022-04-08 19:14:35 +0300

    reabse with google

--- piggy-client/src/views/ChildHomePage/ChildHomePage.jsx
+++ piggy-client/src/views/ChildHomePage/ChildHomePage.jsx
@@ -2,36 +2,41 @@ import * as React from "react";
 import HomePage from "../../components/HomePage";
 import HomepageHeader from "../../components/HomePage/HomepageHeader";
 import HomePageFooter from "../../components/HomePage/HomePageFooter";
+import CardAmount from "../../components/CardInfo/CardHistory";
+import config from '../../conf.json'
 import CardDetails from "../../components/CardInfo/CardDetails";
 import { useEffect, useState } from "react";
+import { makeStyles } from "@mui/styles";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import axios from "axios";
-import config from "../../conf.json";
+import configData from "../../conf.json";
+import ChildrenSettings from "../ChildrenSettings/ChildrenSettings";
+import Router from "../../components/Router";
 import CardHistory from "../../components/CardInfo/CardHistory";
+import routes from "../../components/Router/Routes";
+
+const useStyles = makeStyles((theme) => ({
+  //     root:{
+  //     },
+}));
 
 const ChildHomePage = () => {
+  const classes = useStyles();
   const [cardData, setCardData] = useState(null);
-  const [userName, setUserName] = useState("");
-
   useEffect(async () => {
-    const userMail = JSON.parse(sessionStorage.getItem("profileObj"))["email"];
-    console.log(userMail);
-    const user = await axios.get(
-      `${config.PIGGY_DB_URL}/children/mail/${userMail}`
-    );
-    setUserName(user.data.UserSettings.DisplayName);
-    console.log(user.data);
+    //const userMail = JSON.parse(sessionStorage.getItem("profileObj"))["email"];
+    const userMail = 'mika@gmail.com';
+        console.log(userMail);
+        const user = await axios.get(`${config.PIGGY_DB_URL}/children/mail/${userMail}`); 
+        console.log(user.data);
 
-    const userCard = await axios.get(
-      `${config.PAYMENT_SERVICE_URL}/card/${user.data._id}`
-    );
-    setCardData(userCard.data);
+        const userCard = await axios.get(`${config.PAYMENT_SERVICE_URL}/card/${user.data._id}`);
+        setCardData(userCard.data);
   }, []);
 
   return (
-    <div>
-      <HomePage title="PIGGY" btnText="לתצוגת הורה" btnLink="/parent" />
-      <HomepageHeader username={userName} caption="בוקר אש" />
-
+    <div className={classes.root}>
       <CardDetails
         amount={cardData?.amount}
         details={cardData?.cardDetails}
