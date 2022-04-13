8be1a95e2b2f2a403d71adfc82eb250ff10a182d
Lee Meiri <leemeiri1@gmail.com>
2022-04-14 01:06:09 +0300

    fix login

--- piggy-client/src/components/Router/Router.jsx
+++ piggy-client/src/components/Router/Router.jsx
@@ -1,29 +1,31 @@
-import { React } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import ParentHomePage from '../../views/ParentHomePage';
-import ChildHomePage from '../../views/ChildHomePage';
-import ParentTransferMoneyPage from '../../views/ParentTransferMoneyPage';
-import UserTypeSelect from '../../views/UserTypeSelect';
-import ParentLogin from '../../views/ParentLogin';
-import ChildLogin from '../../views/ChildLogin';
-import ChildrenSettings from '../../views/ChildrenSettings/ChildrenSettings';
-import routes from './Routes';
+// import { React } from 'react';
+// import { Routes, Route, Navigate, Switch } from 'react-router-dom';
+// import ParentHomePage from '../../views/ParentHomePage';
+// import ChildHomePage from '../../views/ChildHomePage';
+// import ParentTransferMoneyPage from '../../views/ParentTransferMoneyPage';
+// import UserTypeSelect from '../../views/UserTypeSelect';
+// import ParentLogin from '../../views/ParentLogin';
+// import ChildLogin from '../../views/ChildLogin';
+// import ChildrenSettings from '../../views/ChildrenSettings/ChildrenSettings';
+// import routes from './Routes';
 
-const Router = () => {
-	return (
-		<Routes>
-			<Route path={routes.Landing} element={<UserTypeSelect />} />
-			<Route path={routes.LoginParent} element={<ParentLogin />} />
-			<Route path={routes.LoginChild} element={<ChildLogin />} />
-			<Route path='/parent' element={<ParentHomePage />} />
-			<Route path='/child' element={<ChildHomePage />} />
-			<Route path={routes.ParentHomePage} element={<ParentHomePage />} />
-			<Route path={routes.ChildrenHomePage} element={<ChildrenIndex />} />
-			<Route path={routes.ChildSettings} element={<ChildrenSettings />} />
-			<Route path={routes.ParentTransfer} element={<ParentTransferMoneyPage />} />
-			<Route path='*' element={<Navigate to={'/landing'} />} />
-		</Routes>
-	);
-};
+// const Router = () => {
+// 	return (<></>
+// 		// <Switch>
+// 		// <Routes>
+// 		// 	<Route path={routes.Landing} element={<UserTypeSelect />} />
+// 		// 	<Route path={routes.LoginParent} element={<ParentLogin />} />
+// 		// 	<Route path={routes.LoginChild} element={<ChildLogin />} />
+// 		// 	<Route path='/parent' element={<ParentHomePage />} />
+// 		// 	{/* <Route path='/child' element={<ChildHomePage />} /> */}
+// 		// 	<Route path={routes.ParentHomePage} element={<ParentHomePage />} />
+// 		// 	<Route path={routes.ChildrenHomePage} element={<ChildrenIndex />} />
+// 		// 	<Route path={routes.ChildSettings} element={<ChildrenSettings />} />
+// 		// 	<Route path={routes.ParentTransfer} element={<ParentTransferMoneyPage />} />
+// 		// 	<Route path='*' element={<Navigate to={'/landing'} />} />
+// 		// </Routes>
+// 		// </Switch>
+// 	);
+// };
 
-export default Router;
+// export default Router;
