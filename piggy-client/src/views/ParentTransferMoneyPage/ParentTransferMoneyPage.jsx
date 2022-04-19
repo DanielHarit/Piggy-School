1b0b75eb72ce45fb8bac6d13ff201c67c72c5902
Revital Gornstein <revital12@meet.mit.edu>
2022-04-19 13:44:24 +0300

    transfer page ready

--- piggy-client/src/views/ParentTransferMoneyPage/ParentTransferMoneyPage.jsx
+++ piggy-client/src/views/ParentTransferMoneyPage/ParentTransferMoneyPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 
-import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
 import Typography from '@mui/material/Typography';
 import makeStyles from '@mui/styles/makeStyles';
 import ChildrenDisplay from './ChildrenDisplay';
@@ -8,11 +8,12 @@ import axios from 'axios';
 import ParentContext from '../../ParentContext';
 import config from '../../conf.json';
 
-import { Grid, Button } from '@mui/material';
+import { Grid, Button, LinearProgress } from '@mui/material';
 import Skeleton from '@mui/material/Skeleton';
 import Swal from 'sweetalert2';
 
 import configData from '../../conf.json';
+import NumbersKeyboard from './NumbersKeyboard';
 
 const useStyles = makeStyles((theme) => ({
 	container: {
@@ -23,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
 	},
 	skelatonContainer: {
 		paddingTop: '15px',
-		paddingBottom: '32px',
+		paddingBottom: '40px',
 	},
 	skelaton: {
 		margin: 'auto',
@@ -37,11 +38,16 @@ const useStyles = makeStyles((theme) => ({
 		justifyContent: 'flex-start',
 		paddingTop: theme.spacing(2),
 	},
+	likeBtn: {
+		height: '36px',
+		borderRadius: '4px',
+	},
 }));
 
 const ParentTransferMoneyPage = ({ parentId }) => {
 	const { amount, setAmount, setSelectedChildrenId, selectedChildrenId } = useContext(ParentContext);
 	const [isChildrenLoading, setIsChildrenLoading] = useState(true);
+	const [isTransferLoading, setIsTransferLoading] = useState(false);
 
 	const classes = useStyles();
 
@@ -57,7 +63,7 @@ const ParentTransferMoneyPage = ({ parentId }) => {
 	}, [parentId]);
 
 	const handleTransferMoney = () => {
-		console.log('payed for ' + selectedChildrenId + ': ' + amount);
+		setIsTransferLoading(true);
 
 		axios
 			.put(`${configData.PAYMENT_SERVICE_URL}/card/${selectedChildrenId}`, {
@@ -82,7 +88,8 @@ const ParentTransferMoneyPage = ({ parentId }) => {
 					confirmButtonColor: '#781f63',
 					confirmButtonText: 'הבנתי',
 				});
-			});
+			})
+			.finally(() => setIsTransferLoading(false));
 	};
 
 	return (
@@ -110,16 +117,20 @@ const ParentTransferMoneyPage = ({ parentId }) => {
 				)}
 			</div>
 
-			<TextField
-				label='כמה תרצה להעביר?'
-				variant='outlined'
-				color='primary'
-				fullWidth
-				value={+amount || ''}
-				type='number'
-				onChange={(event) => setAmount(event.target.value)}
-			/>
-			<Button onClick={handleTransferMoney}>העבר</Button>
+			<NumbersKeyboard />
+			{isTransferLoading ? (
+				<LinearProgress className={classes.likeBtn} />
+			) : parentId && !isChildrenLoading ? (
+				<Button
+					variant='contained'
+					fullWidth
+					onClick={handleTransferMoney}
+					disabled={!selectedChildrenId || amount <= 0}>
+					{`העבר ${amount}₪ ל${childrens.find((child) => child._id === selectedChildrenId)?.UserSettings.DisplayName}`}
+				</Button>
+			) : (
+				<Skeleton className={classes.likeBtn} />
+			)}
 		</div>
 	);
 };
