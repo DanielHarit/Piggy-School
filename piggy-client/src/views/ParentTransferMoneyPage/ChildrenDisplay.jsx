825af3df1035707a6846e24afb76ebef7b523282
Revital Gornstein <revital12@meet.mit.edu>
2022-04-19 12:18:33 +0300

    shows children wish avatars and added skelaton

--- piggy-client/src/views/ParentTransferMoneyPage/ChildrenDisplay.jsx
+++ piggy-client/src/views/ParentTransferMoneyPage/ChildrenDisplay.jsx
@@ -1,54 +1,52 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import makeStyles from "@mui/styles/makeStyles";
-import Paper from "@mui/material/Paper";
-import { IconButton } from "@mui/material";
-import clsx from "clsx";
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import makeStyles from '@mui/styles/makeStyles';
+import { Avatar, IconButton } from '@mui/material';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    paddingTop: "30px",
-    display: "flex",
-    flexDirection: "column",
-    maxWidth: "100px",
-    justifyContent: "center",
-  },
-  childrenDisplay: {
-    borderRadius: "50%",
-    height: "60px",
-    width: "60px",
-  },
-  selected: {
-    border: "1px",
-    borderStyle: "solid",
-    borderWidth: "2px",
-    borderColor: theme.palette.primary.main,
-  },
-  text: {
-    color: theme.palette.primary.main,
-    textAlign: "center",
-  },
+	container: {
+		paddingTop: '7px',
+		display: 'flex',
+		flexDirection: 'column',
+		maxWidth: '100px',
+		justifyContent: 'center',
+	},
+	childrenDisplay: {
+		borderRadius: '50%',
+		height: '60px',
+		width: '60px',
+	},
+	selected: {
+		border: '1px',
+		borderStyle: 'solid',
+		borderWidth: '2px',
+		borderColor: theme.palette.primary.main,
+	},
+	text: {
+		color: theme.palette.primary.main,
+		textAlign: 'center',
+	},
 }));
 
-const ChildrenDisplay = ({ name, onClick, selected }) => {
-  const classes = useStyles();
+const ChildrenDisplay = ({ name, onClick, selected, pic }) => {
+	const classes = useStyles();
 
-  return (
-    <div className={classes.container}>
-      <IconButton onClick={onClick}>
-        <Paper
-          elevation={3}
-          className={clsx({
-            [classes.childrenDisplay]: true,
-            [classes.selected]: selected,
-          })}
-        ></Paper>
-      </IconButton>
-      <Typography variant="h6" component="div" className={classes.text}>
-        {name}
-      </Typography>
-    </div>
-  );
+	return (
+		<div className={classes.container}>
+			<IconButton onClick={onClick}>
+				<Avatar
+					src={pic}
+					className={clsx({
+						[classes.childrenDisplay]: true,
+						[classes.selected]: selected,
+					})}></Avatar>
+			</IconButton>
+			<Typography variant='h6' component='div' className={classes.text}>
+				{name}
+			</Typography>
+		</div>
+	);
 };
 
 export default ChildrenDisplay;
