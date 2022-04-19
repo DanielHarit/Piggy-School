cb3e1e8944e691946e7d09fa52a4b52d8ca32316
Revital Gornstein <revital12@meet.mit.edu>
2022-04-19 13:48:03 +0300

    fix parent page css

--- piggy-client/src/views/ParentIndex.js
+++ piggy-client/src/views/ParentIndex.js
@@ -1,68 +1,68 @@
-import React, { useEffect } from "react";
-import HomePage from "../components/HomePage";
-import makeStyles from "@mui/styles/makeStyles";
-import HomepageHeader from "../components/HomePage/HomepageHeader";
-import HomePageFooter from "../components/HomePage/HomePageFooter";
-import ParentTransferMoneyPage from "./ParentTransferMoneyPage";
-import config from "../conf.json";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import HomePage from '../components/HomePage';
+import makeStyles from '@mui/styles/makeStyles';
+import HomepageHeader from '../components/HomePage/HomepageHeader';
+import HomePageFooter from '../components/HomePage/HomePageFooter';
+import ParentTransferMoneyPage from './ParentTransferMoneyPage';
+import config from '../conf.json';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import axios from "axios";
+import axios from 'axios';
 import ParentContext from '../ParentContext';
-import { HOMEPAGE_CONSTANTS } from "../constants";
-import routes from "../components/Router/Routes";
-import { Routes, Route, Navigate } from "react-router-dom";
-import ParentHomePage from "./ParentHomePage/ParentHomePage";
+import { HOMEPAGE_CONSTANTS } from '../constants';
+import routes from '../components/Router/Routes';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ParentHomePage from './ParentHomePage/ParentHomePage';
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		height: 'calc(100vh - 330px)',
+		overflowY: 'auto',
+	},
+}));
 
 const ParentIndex = () => {
-  const [selectedChildrenId, setSelectedChildrenId] = useState('');
-  const [amount, setAmount] = useState(0);
-  const [user, setUser] = useState(null);
+	const classes = useStyles();
+	const [selectedChildrenId, setSelectedChildrenId] = useState('');
+	const [amount, setAmount] = useState(0);
+	const [user, setUser] = useState(null);
 
-  useEffect(async () =>{
-    const userMail = JSON.parse(sessionStorage.getItem("profileObj"))["email"];
-    // const userMail = "shlomi@gmail.com";
-    const user = await axios.get(`${config.PIGGY_DB_URL}/parent/mail/${userMail}`); 
-    setUser(user.data)
-  },[]);
-  const navigate = useNavigate();
+	useEffect(async () => {
+		const userMail = JSON.parse(sessionStorage.getItem('profileObj'))['email'];
+		// const userMail = "shlomi@gmail.com";
+		const user = await axios.get(`${config.PIGGY_DB_URL}/parent/mail/${userMail}`);
+		setUser(user.data);
+	}, []);
+	const navigate = useNavigate();
 
-  return (
-    <ParentContext.Provider
-      value={{ amount, setAmount, selectedChildrenId, setSelectedChildrenId }}
-    >
-      <HomePage
-        title="PIGGY"
-      >
-        <HomepageHeader username={user?.DisplayName} caption="בוקר טיל" />
-        <HomePageFooter
-         footerType={HOMEPAGE_CONSTANTS.PARENT_FOOTER}
-         onBtnsClick={{
-            left: () => {
-              navigate('/parent' + routes.ParentTransfer, {
-                state: { settings: user.UserSettings, mail: user.Mail },
-              });
-            },
-            middle: () => {
-                navigate('/parent' + routes.ParentHomePage);
-              },
-          }}
-        />
-        <div>
-          <Routes>
-          <Route path={routes.ParentHomePage} element={<ParentHomePage/>} />
-            <Route path={routes.ParentTransfer} element={<ParentTransferMoneyPage parentId={user?._id} />} />
-            <Route
-              path="*"
-              element={<Navigate to={"parent"} />}
-            />
-          </Routes>
-        </div>
-      </HomePage>
-    </ParentContext.Provider>
-  );
+	return (
+		<ParentContext.Provider value={{ amount, setAmount, selectedChildrenId, setSelectedChildrenId }}>
+			<HomePage title='PIGGY'>
+				<HomepageHeader username={user?.DisplayName} caption='בוקר טיל' />
+				<div className={classes.root}>
+					<Routes>
+						<Route path={routes.ParentHomePage} element={<ParentHomePage />} />
+						<Route path={routes.ParentTransfer} element={<ParentTransferMoneyPage parentId={user?._id} />} />
+						<Route path='*' element={<Navigate to={'parent'} />} />
+					</Routes>
+				</div>
+				<HomePageFooter
+					footerType={HOMEPAGE_CONSTANTS.PARENT_FOOTER}
+					onBtnsClick={{
+						left: () => {
+							navigate('/parent' + routes.ParentTransfer, {
+								state: { settings: user.UserSettings, mail: user.Mail },
+							});
+						},
+						middle: () => {
+							navigate('/parent' + routes.ParentHomePage);
+						},
+					}}
+				/>
+			</HomePage>
+		</ParentContext.Provider>
+	);
 };
 
 export default ParentIndex;
