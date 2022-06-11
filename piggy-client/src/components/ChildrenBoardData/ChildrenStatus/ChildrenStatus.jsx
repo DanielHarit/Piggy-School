c6e36aa99d7ea07c5ffa4dca2f758c24fbd6f99f
Revital Gornstein <revital12@meet.mit.edu>
2022-06-11 13:18:48 +0300

    fix children quick view numbers

--- piggy-client/src/components/ChildrenBoardData/ChildrenStatus/ChildrenStatus.jsx
+++ piggy-client/src/components/ChildrenBoardData/ChildrenStatus/ChildrenStatus.jsx
@@ -1,82 +1,83 @@
-import { Typography } from '@mui/material'
-import { Divider } from '@mui/material'
+import { Typography } from '@mui/material';
+import { Divider } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { Card, List,Box } from '@mui/material'
-import { makeStyles } from '@mui/styles'
-import PropTypes from 'prop-types'
-import LinearProgress, { LinearProgressProps,linearProgressClasses } from '@mui/material/LinearProgress';
+import { Card, List, Box } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import PropTypes from 'prop-types';
+import LinearProgress, { LinearProgressProps, linearProgressClasses } from '@mui/material/LinearProgress';
 import axios from 'axios';
 import config from '../../../conf.json';
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo } from 'react';
 
 const useStyles = makeStyles((theme) => ({
-  root:{
-  },
-   childText:{
-    fontSize:'20px'
-   },
-   targetText:{
-    fontSize:'15px',
-    color:theme.primary
-   }
- 
-  }))
+	root: {},
+	childText: {
+		fontSize: '20px',
+	},
+	targetText: {
+		fontSize: '15px',
+		color: theme.primary,
+	},
+}));
 
-  const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-    height: 30,
-    borderRadius: 10,
-    [`&.${linearProgressClasses.colorPrimary}`]: {
-      backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
-    },
-    [`& .${linearProgressClasses.bar}`]: {
-      borderRadius: 10,
-      backgroundColor: theme.primary,
-    },
-  }));
-  
-  const ChildrenStatus = ({child, lastDate}) => {
-    const classes = useStyles()
-    const [cardData, setCardData] = useState(null);
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+	height: 30,
+	borderRadius: 10,
+	[`&.${linearProgressClasses.colorPrimary}`]: {
+		backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+	},
+	[`& .${linearProgressClasses.bar}`]: {
+		borderRadius: 10,
+		backgroundColor: theme.primary,
+	},
+}));
 
-    const userExpenses = useMemo(()=>cardData?.transactions
-    .filter((tran) => (
-      new Date(tran.timestamp) >= lastDate
-     ))
-    .map((transaction) => transaction.amount).reduce((a, b) => a + b, 0),[cardData]);
-    
-    const percent = useMemo(()=>cardData?.amount && Math.min(userExpenses / (cardData?.amount + userExpenses)* 100, 100),[userExpenses,cardData]);
-   
-    useEffect(async () => {
-          const userCard = await axios.get(`${config.PAYMENT_SERVICE_URL}/card/${child._id}`);
-          setCardData(userCard.data);
-    }, []);
+const ChildrenStatus = ({ child, lastDate }) => {
+	const classes = useStyles();
+	const [cardData, setCardData] = useState(null);
+	const [userBudget, setUserBudget] = useState(0);
 
-    return (
-      <div className={classes.root}>
-        <Box sx={{display:'flex',justifyContent: 'space-between',width:'100%'}}>
-                <Typography className={classes.childText} > {child.UserSettings.DisplayName} </Typography>
-                <Typography className={classes.targetText} >  
-                ₪ {cardData?.amount + userExpenses} /  
-                  {userExpenses}
-                </Typography>
-              </Box>
-              <Box sx={{ width: '100%'}}>
-              <BorderLinearProgress 
-                variant="determinate" 
-            value={percent || 0}
-            />
-        </Box>
-    </div>
-    )
-  }
-  
-  ChildrenStatus.propTypes = {
-    child:PropTypes.object
-  }
-  ChildrenStatus.defaultProps = {
-    child:{}
-  }
-  
-  
-  export default ChildrenStatus
-  
+	const userExpenses = useMemo(
+		() =>
+			cardData?.transactions
+				.filter((tran) => new Date(tran.timestamp) >= lastDate)
+				.map((transaction) => transaction.amount)
+				.reduce((a, b) => a + b, 0),
+		[cardData]
+	);
+
+	const percent = useMemo(
+		() => cardData?.amount && Math.min((userExpenses / (cardData?.amount + userExpenses)) * 100, 100),
+		[userExpenses, cardData]
+	);
+
+	useEffect(async () => {
+		const userCard = await axios.get(`${config.PAYMENT_SERVICE_URL}/card/${child._id}`);
+		setCardData(userCard.data);
+		const userTotalBudget = Object.values(child.Budget).reduce((sum, categoryAmount) => sum + +categoryAmount, 0);
+		setUserBudget(userTotalBudget);
+	}, []);
+
+	return (
+		<div className={classes.root}>
+			<Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+				<Typography className={classes.childText}> {child.UserSettings.DisplayName} </Typography>
+				<Typography className={classes.targetText}>
+					{/* ₪ {cardData?.amount + userExpenses} /{userExpenses} */} {userExpenses}/{userBudget}₪
+				</Typography>
+			</Box>
+			<Box sx={{ width: '100%' }}>
+				<BorderLinearProgress variant='determinate' value={percent || 0} />
+			</Box>
+		</div>
+	);
+};
+
+ChildrenStatus.propTypes = {
+	child: PropTypes.object,
+};
+ChildrenStatus.defaultProps = {
+	child: {},
+};
+
+export default ChildrenStatus;
